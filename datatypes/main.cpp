31fb8e9cdc7547da7982785605ab0a4524757ea3
Tanjil <tanjilxd@gmail.com>
2020-06-05 19:09:41 +0100

    Add files via upload

---
+++ datatypes/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main(){
+
+    short a;
+    int b;
+    long c;
+    long long d;
+    // short <= int <= long <= long long
+    unsigned short aa;
+    unsigned int bb;
+    unsigned long cc;
+    unsigned long long dd;
+
+    cout << ULLONG_MAX << std::endl;
+}
